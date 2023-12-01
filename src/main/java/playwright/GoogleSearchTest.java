36999e0d0f1f33c4e63b1d9fd7a6e7060297e32a
Hardik Patel <hrdkptl96@gmail.com>
2023-12-01 13:39:54 -0400

    Added com.deque.html.axe-core.playwright
    Tested Accessiblity testing in GoogleSearchTest

--- src/main/java/playwright/GoogleSearchTest.java
+++ src/main/java/playwright/GoogleSearchTest.java
@@ -1,11 +1,19 @@
 package playwright;
 
+import java.util.Arrays;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.deque.html.axecore.playwright.AxeBuilder;
+import com.deque.html.axecore.results.AxeResults;
+import com.deque.html.axecore.results.Check;
+import com.deque.html.axecore.results.CheckedNode;
+import com.deque.html.axecore.results.Rule;
 import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
 
 public class GoogleSearchTest extends Parent {
 
@@ -18,11 +26,17 @@ public class GoogleSearchTest extends Parent {
 
     @Test
     public void performSearch() {
-        //page.fill("//textarea[@title='Search']", "OpenAI" + Keys.ENTER);
+    	
+    	checkAccessibility(page);
 
-        page.fill("//textarea[@title='Search']", "OpenAI");
-        page.click("(//input[@value='Google Search' and @role='button' and @type='submit'])[2]");
-
+        Locator inputField =  page.locator("//textarea[@title='Search']");
+        inputField.fill("OpenAI");
+        inputField.press("Enter");
+        
+        //page.fill("//textarea[@title='Search']", "OpenAI");
+        //page.click("(//input[@value='Google Search' and @role='button' and @type='submit'])[2]");
+        
+        System.out.println("################# NEW PAGE #######################");
         // Wait for the results stats to be visible
         ElementHandle resultsStats = page.waitForSelector("#result-stats");
         String resultsText = resultsStats.innerText();
@@ -35,6 +49,9 @@ public class GoogleSearchTest extends Parent {
 
         // Check if the number of results is greater than 100,000,000
         Assert.assertTrue(numberOfResults > 100000000, "Number of results is not greater than 100,000,000");
+        
+        checkAccessibility(page);
+
     }
 
     @AfterMethod
@@ -42,5 +59,36 @@ public class GoogleSearchTest extends Parent {
         if (page != null) {
             page.close();
         }
+    }
+    
+    public void checkAccessibility(Page page) {
+    	
+    	AxeResults accessibilityScanResults = new AxeBuilder(page)
+  			  .withTags(Arrays.asList("wcag2a", "wcag2aa", "wcag21a", "wcag21aa"))
+  			  .analyze();
+
+        if (!accessibilityScanResults.getViolations().isEmpty()) {
+            System.out.println("Accessibility Violations:");
+            for (Rule violation : accessibilityScanResults.getViolations()) {
+                System.out.println("Violation Description: " + violation.getDescription());
+                System.out.println("Impact: " + violation.getImpact());
+                System.out.println("Help URL: " + violation.getHelpUrl());
+                System.out.println("Affected Nodes:");
+                for (CheckedNode node : violation.getNodes()) {
+                    System.out.println("  HTML Element: " + node.getHtml());
+                    System.out.println("  Fix all of the following:");
+                    for (Check check : node.getAll()) {
+                        System.out.println("    " + check.getMessage());
+                    }
+                    System.out.println("  Fix any of the following:");
+                    for (Check check : node.getAny()) {
+                        System.out.println("    " + check.getMessage());
+                    }
+                }
+                System.out.println("-----------------------------------------");
+            }
+        }
+        
+        //Assert.assertEquals(accessibilityScanResults.getViolations(), Collections.emptyList());
     }
 }
